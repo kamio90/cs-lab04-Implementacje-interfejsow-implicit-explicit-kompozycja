1e22a122286dd24f6d7fcc2cdb5521ee603c328a
Kamil Musiał <kmusial40@gmail.com>
2020-12-15 22:14:11 +0100

    property Counter zwraca liczbę uruchomień kserokopiarki

--- Zadanie1/Copier.cs
+++ Zadanie1/Copier.cs
@@ -4,6 +4,13 @@ namespace Zadanie1
     {
         private int _printCounter;
         private int _scanCounter;
+        private int _counter;
+
+        public int Counter
+        {
+            get => _counter;
+            set => _counter = value;
+        }
 
         public int PrintCounter
         {
