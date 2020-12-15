96f5d14976837562438e4499496ed7eb87cf0277
Kamil Musiał <kmusial40@gmail.com>
2020-12-15 22:13:44 +0100

    property ScanCounter zwraca liczbę zeskanowanych dokumentów,

--- Zadanie1/Copier.cs
+++ Zadanie1/Copier.cs
@@ -3,8 +3,18 @@ namespace Zadanie1
     public class Copier
     {
         private int _printCounter;
+        private int _scanCounter;
 
-        public int PrintCounter => _printCounter;
+        public int PrintCounter
+        {
+            get => _printCounter;
+            set => _printCounter = value;
+        }
 
+        public int ScanCounter
+        {
+            get => _scanCounter;
+            set => _scanCounter = value;
+        }
     }
 }
